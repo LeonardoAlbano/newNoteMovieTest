40545ab6b9c1f33d8b157da011908e001bddef5d
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2023-11-30 13:02:34 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { New } from './pages/New';
+
+
+// Importando o themeProvider da libe style-com..
+// Importando o theme da pasta styles/theme.js 
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme'
+
+// Importar os styles globals 
+import GlobalStyles from './styles/global';
+import { Home } from './pages/Home';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <New />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
